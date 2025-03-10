71dbd7087e19c6501e37d918987bf0fc6798a84f
Inho Lee <harrylee0116@gmail.com>
2025-03-10 14:50:28 +0900

    feat. add camera button

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 document.getElementById("gps-button").addEventListener("click", () => {
     window.webkit.messageHandlers.GPS.postMessage("gps");
 });
+
+document.getElementById("camera-button").addEventListener("click", () => {
+    window.webkit.messageHandlers.CAMERA.postMessage("camera");
+});
