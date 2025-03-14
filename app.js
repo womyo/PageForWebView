dde0c1b4207e1a1a4e9eedf25b98a000ff5282e5
Inho Lee <harrylee0116@gmail.com>
2025-03-14 16:15:28 +0900

    refactor. erase test function

--- app.js
+++ app.js
@@ -10,8 +10,3 @@ document.getElementById("cameraN-button").addEventListener("click", () => {
 document.getElementById("cameraW-button").addEventListener("click", () => {
     window.webkit.messageHandlers.CAMERA_W.postMessage("cameraWeb");
 });
-
-function test() {
-    const displayText = document.getElementById("displayText");
-    displayText.textContent = "Bye";
-}
