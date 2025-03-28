d8ac4b11bd25b1454433c9f383c1f1ab5c52fbbd
Inho Lee <harrylee0116@gmail.com>
2025-03-28 15:40:27 +0900

    feat. add file save, read button

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ document.getElementById("cameraN-button").addEventListener("click", () => {
 document.getElementById("cameraW-button").addEventListener("click", () => {
     window.webkit.messageHandlers.CAMERA_W.postMessage("cameraWeb");
 });
+
+document.getElementById("fileSave-button").addEventListener("click", () => {
+    window.webkit.messageHandlers.FILE_SAVE.postMessage("fileSave");
+});
+
+document.getElementById("fileRead-button").addEventListener("click", () => {
+    window.webkit.messageHandlers.FILE_READ.postMessage("fileRead");
+});
